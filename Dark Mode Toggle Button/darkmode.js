b42b8ce2bedbf399e8bf8e0a4c0eaeb8423ea0f4
vishal248 <vishalkumar993911@gmail.com>
2025-09-18 16:53:05 +0530

    fix

--- Dark Mode Toggle Button/darkmode.js
+++ Dark Mode Toggle Button/darkmode.js
@@ -8,13 +8,15 @@ btn.addEventListener("click", ()=>{
     document.querySelector(".container").style.backgroundColor="black";
     document.querySelector(".button").style.backgroundColor="white";
     document.getElementById("heading").style.color = "white";
-    document.getElementById("heading").textContent = "DARK";
+    document.getElementById("heading").textContent = "DARK MODE"
+    
   }else{
     currMode="light";
     document.querySelector(".container").style.backgroundColor="white";
     document.querySelector(".button").style.backgroundColor="#d2d2d2";
     document.getElementById("heading").style.color = "black";
-    document.getElementById("heading").textContent = "LIGHT";
+    document.getElementById("heading").textContent = "LIGHT MODE";
+    document.getElementById("heading").innerHTML=<i class="fa fa-sun-o" aria-hidden="true"></i>
   }
 
   console.log(currMode);
